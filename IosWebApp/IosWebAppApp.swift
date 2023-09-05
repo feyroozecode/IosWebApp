53bba981f407a5ec63d1b299959b0b46217df323
ibrahmad <ibrahmad18@gmail.com>
2023-09-05 18:40:15 +0100

    Initial Commit

---
+++ IosWebApp/IosWebAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosWebAppApp.swift
+//  IosWebApp
+//
+//  Created by macbook air on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct IosWebAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
